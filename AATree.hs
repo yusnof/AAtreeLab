c1c9985a2e4ab62b860fb5915df66039cbb970f4
Yousef <36880618+yusnof@users.noreply.github.com>
2024-05-06 15:00:15 +0200

    yes done Co-authored-by: GunnarJohansson <GunnarJohansson@users.noreply.github.com> Co-authored-by: Markus-Havner <Markus-Havner@users.noreply.github.com>

--- AATree.hs
+++ AATree.hs
@@ -18,45 +18,14 @@ module AATree (
  ) where
 
 
-
-
-
-
-
-
 --------------------------------------------------------------------------------
 
 -- AA search trees
 data AATree a = Empty | Node { level :: Int,  left :: AATree a,  value :: a,  right :: AATree a }
---2 or node
   deriving (Eq, Show, Read)
 
 emptyTree :: AATree a
 emptyTree = Empty 
-
-test22 = Empty
-
-testTree= Node 2 (Node 1 (Node 2 Empty 1000 Empty) 2000 (Node 1 Empty 3000 Empty)) 4000 (Node 1 Empty 5000 Empty)
-testSkew= skew(testTree)
-
-
-
-test :: AATree Integer
-test = Node 1 (Node 1 Empty 1 Empty) 2 (Node 1 Empty 3 Empty) 
--- insert 0 test : 
-
-test1 :: AATree Integer 
-test1 = Node 3 (Node 2 Empty 2 Empty) 7 (Node 3 Empty 15 Empty)
-
-test3 :: AATree Integer
-test3 = Node 2 (Node 1 Empty 2 Empty) 15 (Node 2 Empty 7 Empty)
-
---testSkew :: Bool
---testSkew | skew (test3) == Node 1 
-testList :: [Integer]
-testList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,3,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-
-
 
 get :: Ord a => a -> AATree a -> Maybe a
 get _ Empty = Nothing
@@ -65,7 +34,6 @@ get x (Node _ l c r)
   | x > c = get x r 
   | x < c = get x l
    
-
 -- the four node x -> y -> z should be restructured so that y is moved up.
 --              /    /    /\
 -- x and z becomes the l and r  children of y (respectively). 
@@ -73,66 +41,50 @@ get x (Node _ l c r)
 -- z keeps its children.
 -- in order for the pattern-match to work, split needs to be called at the leftmost node in the 4-node (x in the example above).
 split :: AATree a -> AATree a
-split (Node levelx lx cx (Node levely ly cy (Node levelz lz cz rz))) = Node (levely+1)
- (Node levelx lx cx ly) 
- cy
-  (Node levelz lz cz rz)
+split (Node levelx lx cx (Node levely ly cy (Node levelz lz cz rz))) 
+  | (levelx == levely) && (levely == levelz) = --Determine that the four node is created
+  Node (levely+1) (Node levelx lx cx ly) cy (Node levelz lz cz rz)
 split a = a
 
---vr = value right
+--rr = value right
 --vl = value left and so on..
 --skew  :: AATree a -> AATree a
---skew (Node value (Node vr rl rc rr) center (Node vl ll lc lr)) = 
---   Node value (Node vr rl rc (Node vl ll lc lr)) center (Node value rr lc lr)
---
 --    x <- y    becomes   x -> y
 --   /\    \             /    /\
 --  A  B    C           A     B C
-skew  :: AATree a -> AATree a
-skew (Node levelY (Node levelX leftX valueX rightX ) valueY (Node levelC Empty valueC Empty)) = 
-  Node levelX leftX valueX (Node levelY rightX valueY (Node levelC Empty valueC Empty))
-skew t = t 
+
+skew :: AATree a -> AATree a
+skew Empty = Empty
+skew a = a
+skew (Node lvl Empty v r) = Node lvl Empty v r
+skew (Node lvl (Node lvla l v rr) y r) | lvl == lvla = Node lvla l v (Node lvl rr y r)
 
 
-
-
--- and call these from insert.
--- the 3 cases 
+--
 insert :: Ord a => a -> AATree a -> AATree a
-insert x Empty = Node 1 Empty x Empty
+insert x Empty = Node 1 Empty x Empty --Base case
+insert x y@(Node level left value right) --
+ | x < value = helpf(Node level (insert x left) value right)
+ | x > value = helpf(Node level left value (insert x right))
+ | otherwise = y -- do nothing when adding the same element 
+  where 
+    helpf x = split(skew x) 
 
-insert x (Node level Empty value Empty) =  Node level (Node level Empty x Empty) value Empty --y@
-
-insert x (Node level Empty value (Node l Empty v Empty)) -- Has right child but no grandchildren
-  | x < value = split (skew (Node level (Node level Empty x Empty) value (Node l Empty v Empty)))  -- here we should check for skew
-  | otherwise = (Node level Empty value (insert x  (Node l Empty v Empty)))
-
-insert x (Node level left value Empty)
-  | x > value = (Node level left value (Node level Empty x Empty))
-  | otherwise = (Node level (insert x left) value Empty)
-
-insert x (Node level left value right) 
- | x < value = Node level (insert x left) value right 
- | x > value = Node level left value (insert x right)
- | otherwise = error "insert: Can't insert duplicate values"
-    
-
-
+--Returns list of 
 inorder :: AATree a -> [a]
 inorder Empty = []
 inorder (Node _ Empty x Empty) = [x] 
 inorder (Node _ l c r) = inorder l ++ [c] ++ inorder r
 
+-- returns the size/amount of node in a tree 
 size :: AATree a -> Int
 size Empty = 0
 size (Node _ l c r) = 1 + size l + size r 
 
-
+-- returns the number of levels / height of tree
 height :: AATree a -> Int
 height Empty = 0
 height (Node level l c r) = level 
-
-
 
 --------------------------------------------------------------------------------
 -- Optional function
@@ -140,12 +92,9 @@ height (Node level l c r) = level
 remove :: Ord a => a -> AATree a -> AATree a
 remove emptyTree x = x
 remove x y = error "Error in remove func" 
-
-
   
 --------------------------------------------------------------------------------
 -- Check that an AA tree is ordered and obeys the AA invariants
-
 checkTree :: Ord a => AATree a -> Bool
 checkTree root =
   isSorted (inorder root) &&
@@ -162,22 +111,29 @@ isSorted [x] = True
 isSorted (x:y:xs) = x < y && isSorted xs  
   
 -- Check if the invariant is true for a single AA node
--- A node's level must be greater than its left child: level(node) > level(node.left)
--- And also greater than its right-right grandchild: level(node) > level(node.right.right)
+-- You may want to write this as a conjunction e.g.
+--   checkLevels node =
+--     leftChildOK node &&
+--     rightChildOK node &&
+--     rightGrandchildOK node
+-- where each conjunct checks one aspect of the invariant
 checkLevels :: AATree a -> Bool
-checkLevels (Node level Empty c Empty) = level == 1 -- we need to check when rightSub and leftSub is Empty
-checkLevels (Node level left c right) = level >h height left && level > height (rightSub right)
+checkLevels Empty = True
+checkLevels (Node level left _ right) = 
+  level > height left &&
+  level >= height right &&
+  level > height (rightSub right)
 
- 
 isEmpty :: AATree a -> Bool
 isEmpty Empty = True
 isEmpty _ = False 
 
-
+-- returns a left tree subtree 
 leftSub :: AATree a -> AATree a
 leftSub Empty           = Empty
 leftSub (Node _ l c r ) = l
 
+-- returns a right tree subtree 
 rightSub :: AATree a -> AATree a
 rightSub Empty           = Empty 
 rightSub (Node _ l c r ) = r 
