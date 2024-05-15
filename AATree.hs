fb800ceeffd7c37ad7ce0f264666bb8fac577318
Yousef <36880618+yusnof@users.noreply.github.com>
2024-05-15 11:03:18 +0200

    yes Co-authored-by: GunnarJohansson <GunnarJohansson@users.noreply.github.com>

--- AATree.hs
+++ AATree.hs
@@ -1,7 +1,4 @@
 {-# OPTIONS -Wall #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
 --------------------------------------------------------------------------------
 
@@ -11,12 +8,10 @@ module AATree (
   get,           -- Ord a => a -> AATree a -> Maybe a
   insert,        -- Ord a => a -> AATree a -> AATree a
   inorder,       -- AATree a -> [a]
-  remove,        -- Ord a => a -> AATree a -> AATree a
   size,          -- AATree a -> Int
   height,        -- AATree a -> Int
   checkTree      -- Ord a => AATree a -> Bool
  ) where
-
 
 --------------------------------------------------------------------------------
 
@@ -27,74 +22,86 @@ data AATree a = Empty | Node { level :: Int,  left :: AATree a,  value :: a,  ri
 emptyTree :: AATree a
 emptyTree = Empty 
 
+{- 
+Returns an element from the tree.
+O(log n)
+-}
 get :: Ord a => a -> AATree a -> Maybe a
 get _ Empty = Nothing
 get x (Node _ l c r) 
   | x == c = Just c 
   | x > c = get x r 
   | x < c = get x l
+  | otherwise = Nothing 
+
    
--- the four node x -> y -> z should be restructured so that y is moved up.
---              /    /    /\
--- x and z becomes the l and r  children of y (respectively). 
--- x left child is unchanged, x right child was y's left child.
--- z keeps its children.
--- in order for the pattern-match to work, split needs to be called at the leftmost node in the 4-node (x in the example above).
+{-
+  We compare the levels of the children to the right of the input 
+  Node and if we have the case when we created a three node then we split the tree 
+  O(1)
+-}
 split :: AATree a -> AATree a
 split (Node levelx lx cx (Node levely ly cy (Node levelz lz cz rz))) 
-  | (levelx == levely) && (levely == levelz) = --Determine that the four node is created
+  | (levelx == levely) && (levely == levelz) = --Determine that the fourth node is created
   Node (levely+1) (Node levelx lx cx ly) cy (Node levelz lz cz rz)
 split a = a
 
---rr = value right
---vl = value left and so on..
---skew  :: AATree a -> AATree a
---    x <- y    becomes   x -> y
---   /\    \             /    /\
---  A  B    C           A     B C
-
+{- 
+If the input tree is empty, return empty
+If the input tree has a empty child we insert
+If the input tree has a empty child and lvl equals lvla 
+      x <- y    becomes   x -> y
+     /\    \             /    /\
+    A  B    C           A     B C 
+Otherwise we return input
+O(1) 
+-}
 skew :: AATree a -> AATree a
 skew Empty = Empty
-skew a = a
 skew (Node lvl Empty v r) = Node lvl Empty v r
 skew (Node lvl (Node lvla l v rr) y r) | lvl == lvla = Node lvla l v (Node lvl rr y r)
+skew a = a
 
-
---
+{-
+The base case is inserting a element into an empty tree. 
+In the other cases we compare the value with inserted element and based on the statment, inserting in the right or left Node after that
+with the help of the helper function skew and split. 
+In the case of equal value then we do nothing.
+In every case we insert a value we call on split and skew
+O(log n)
+-}
 insert :: Ord a => a -> AATree a -> AATree a
 insert x Empty = Node 1 Empty x Empty --Base case
-insert x y@(Node level left value right) --
- | x < value = helpf(Node level (insert x left) value right)
- | x > value = helpf(Node level left value (insert x right))
- | otherwise = y -- do nothing when adding the same element 
+insert x y@(Node level left value right) 
+ | x < value = helpf (Node level (insert x left) value right) --if inserted value is bigger then existing value, insert into left node.
+ | x > value = helpf (Node level left value (insert x right)) --if inserted value is smaller then existing value, insert into right node.
+ | otherwise = y --Case of same value as existing, return the inserted input to avoid duplicates 
   where 
-    helpf x = split(skew x) 
+    helpf tree = split (skew tree)
 
---Returns list of 
+
+-- Returns a inorder list from a tree
+-- O(n)  
 inorder :: AATree a -> [a]
 inorder Empty = []
 inorder (Node _ Empty x Empty) = [x] 
 inorder (Node _ l c r) = inorder l ++ [c] ++ inorder r
 
 -- returns the size/amount of node in a tree 
+-- O(n)
 size :: AATree a -> Int
 size Empty = 0
-size (Node _ l c r) = 1 + size l + size r 
+size (Node _ l _ r) = 1 + size l + size r 
 
 -- returns the number of levels / height of tree
+-- O(1)
 height :: AATree a -> Int
 height Empty = 0
-height (Node level l c r) = level 
-
---------------------------------------------------------------------------------
--- Optional function
-
-remove :: Ord a => a -> AATree a -> AATree a
-remove emptyTree x = x
-remove x y = error "Error in remove func" 
+height (Node _ left _ right) = 1 + max (height left) (height right) 
   
 --------------------------------------------------------------------------------
 -- Check that an AA tree is ordered and obeys the AA invariants
+-- O(n) to be accurate O(3n)
 checkTree :: Ord a => AATree a -> Bool
 checkTree root =
   isSorted (inorder root) &&
@@ -105,18 +112,22 @@ checkTree root =
       | otherwise = x:nodes (leftSub x) ++ nodes (rightSub x)
 
 -- True if the given list is ordered
+-- O(n)
 isSorted :: Ord a => [a] -> Bool
 isSorted [] = True
-isSorted [x] = True
+--isSorted [x] = True
 isSorted (x:y:xs) = x < y && isSorted xs  
+isSorted _ = True  
   
--- Check if the invariant is true for a single AA node
+{- Check if the invariant is true for a single AA node
 -- You may want to write this as a conjunction e.g.
 --   checkLevels node =
 --     leftChildOK node &&
 --     rightChildOK node &&
 --     rightGrandchildOK node
 -- where each conjunct checks one aspect of the invariant
+O(n)
+-}
 checkLevels :: AATree a -> Bool
 checkLevels Empty = True
 checkLevels (Node level left _ right) = 
@@ -124,19 +135,22 @@ checkLevels (Node level left _ right) =
   level >= height right &&
   level > height (rightSub right)
 
+--O(1)
 isEmpty :: AATree a -> Bool
 isEmpty Empty = True
 isEmpty _ = False 
 
 -- returns a left tree subtree 
+--O(1)
 leftSub :: AATree a -> AATree a
 leftSub Empty           = Empty
-leftSub (Node _ l c r ) = l
+leftSub (Node _ l _ _ ) = l
 
--- returns a right tree subtree 
+-- returns a right tree subtree
+--O(1) 
 rightSub :: AATree a -> AATree a
 rightSub Empty           = Empty 
-rightSub (Node _ l c r ) = r 
+rightSub (Node _ _ _ r ) = r 
 
 --------------------------------------------------------------------------------
 
--- Main.hs
+++ Main.hs
@@ -27,30 +27,18 @@ printStatistics:: AATree String -> IO ()
 printStatistics tree = do
   let s= size tree
   let h = height tree
-  let optimalHeight= (logBase 2 (fromIntegral (s+1)))-1 -- optimal height: [log(n+1)]-1
+  let optimalHeight = logBase 2 (fromIntegral (s+1))-1 -- optimal height: [log(n+1)]-1
   let optimalHeightRatio = fromIntegral h / optimalHeight
   putStrLn ("Size: "++ show s)
   putStrLn ("Height: "++show h)
   putStrLn ("Optimal height: "++ show optimalHeight)
   putStrLn ("height / optimal heigth: "++ show optimalHeightRatio)
-  putStrLn (show (checkTree tree))
+  print    (checkTree tree)
   putStrLn ("First 20 words: "++ first20 tree)
       
-
-
+first20 :: AATree String -> [Char]
 first20 tree = format (take 20 (inorder tree))
   where
     format [] = ""
     format [x] = x
-    format (x:xs) = x ++" "++ format xs 
-  
-  
-
-
--- optimal height: [log(n+1)]-1
-  -- calculate and print statistics
-  -- use fromIntegral/ceiling/logBase
-  --undefined
-
---------------------------------------------------------------------------------
-
+    format (x:xs) = x ++ " " ++ format xs 
